3e8873cee7abf20b562c13b976b1667dde5032e9
JesusHilario99 <Hilario.juort99@gmail.com>
2023-12-22 16:47:56 -0600

    Seteando la informacion en los tv

--- app/src/main/java/com/example/myapplicationcurso/Constantes.kt
+++ app/src/main/java/com/example/myapplicationcurso/Constantes.kt
@@ -1,8 +1,20 @@
 package com.example.myapplicationcurso
 
+import java.text.DateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.*
+
 object Constantes {
 
     fun verTiempoDis(): Long {
         return System.currentTimeMillis()
+    }
+
+    fun obtenerFecha(tiempo:Long) : String{
+        val calendario = Calendar.getInstance(Locale.ENGLISH)
+        calendario.timeInMillis = tiempo
+
+        return DateFormat.format("dd/MM/yyyy",calendario).toString()
     }
 }
--- app/src/main/java/com/example/myapplicationcurso/fragmentos/FragmentCuenta.kt
+++ app/src/main/java/com/example/myapplicationcurso/fragmentos/FragmentCuenta.kt
@@ -7,11 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.myapplicationcurso.Constantes
 import com.example.myapplicationcurso.OpcionesdeLogin
 import com.example.myapplicationcurso.R
 import com.example.myapplicationcurso.databinding.ActivityLoginEmailBinding
 import com.example.myapplicationcurso.databinding.FragmentCuentaBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.core.Context
 
 class FragmentCuenta : Fragment() {
@@ -34,11 +39,48 @@ class FragmentCuenta : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
+
+        leerinfo()
+
+
+
         binding.btnCerrarSesion.setOnClickListener{
             firebaseAuth.signOut()
             startActivity(Intent(mContext,OpcionesdeLogin::class.java))
             activity?.finishAffinity()
         }
+    }
+
+    private fun leerinfo() {
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val email = "${snapshot.child("email").value}"
+                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
+                    val f_nac = "${snapshot.child("fecha_nac").value}"
+                    var tiempo = "${snapshot.child("tiempo").value}"
+                    val telefono = "${snapshot.child("telefono").value}"
+                    val coodTelefono = "${snapshot.child("codigoTelefono").value}"
+
+                    val cod_tel = coodTelefono+telefono
+
+                    if(tiempo == null){
+                        tiempo = "0"
+                    }
+
+                    val for_tiempo = Constantes.obtenerFecha(tiempo.toLong())
+
+                    //Seteamos la informacion
+                    binding.tvEmail.text = email
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
     }
 
 }
